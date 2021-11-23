7c814795fb10c7e15c9929e618fb336f5b8f783a
yangliuancn <yangliuan@foxmail.com>
2021-11-23 23:43:31 +0800

    add

---
+++ app/Http/Controllers/ExcelCase/OtherController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\ExcelCase;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class OtherController extends Controller
+{
+    /**
+     * https://learnku.com/articles/33769
+     *
+     * @return void
+     */
+    public function export()
+    {
+    }
+}
