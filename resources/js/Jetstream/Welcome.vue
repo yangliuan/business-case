409f775bdf0a1f13b99920cbb25ae7ee0e15cf38
yangliuan <yangliuancn@foxmail.com>
2021-11-24 13:45:15 +0800

    modify

--- resources/js/Jetstream/Welcome.vue
+++ resources/js/Jetstream/Welcome.vue
@@ -25,9 +25,9 @@
                     <div class="mt-2 text-sm text-gray-500">
                         Excel常用导出导出功能
                     </div>
-                    <a href="/api/excel/export/download">
+                    <a href="/api/excel/export/store">
                         <div class="mt-3 flex items-center text-sm font-semibold text-indigo-700">
-                                <div>下载导出Download</div>
+                                <div>导出到磁盘</div>
                                 <div class="ml-1 text-indigo-500">
                                     <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                                 </div>
