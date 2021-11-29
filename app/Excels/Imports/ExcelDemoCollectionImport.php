572e0b36c8ac9aa6460f3a1a9bd074e97a039bf1
yangliuancn <yangliuan@foxmail.com>
2021-11-30 00:29:59 +0800

    modify

--- app/Excels/Imports/ExcelDemoCollectionImport.php
+++ app/Excels/Imports/ExcelDemoCollectionImport.php
@@ -19,7 +19,7 @@ class ExcelDemoCollectionImport implements ToCollection, WithChunkReading, WithH
      */
     public function collection(Collection $rows)
     {
-        $handle_method = 'one_by_one';
+        $handle_method = 'batch_insert';
 
         if ($handle_method == 'one_by_one') {
             //一条接一条导入，适用于复杂关联表的场景
@@ -40,8 +40,25 @@ class ExcelDemoCollectionImport implements ToCollection, WithChunkReading, WithH
             $column_map = [
                 '字符串字段'=>'str_column','整数字段'=>'int_column','浮点数字段'=>'float_column','图片'=>'pic_column','文本字段'=>'text_column'
             ];
-
-            ExcelDemo::insert($this->covertRowData($rows, $column_map)->toArray());
+            $rows = $this->covertRowData($rows, $column_map);
+            //写入数据，注意chunkSize的数值不能太大，导致inset into语句 数据包过大报错
+            //测试用例23567条数据，内存4096M
+            //chunkSize=1 nginx504;废除
+            //chunkSize=100
+            //first=>postman:6m20.82s,telescope:380723ms|second=>postman:6m21.78s,telescope:381700ms|3rd=>postman:6m28.50s,telescope:388458ms
+            //chunksize=800
+            //first=>postman:56.54s,telescope:56495ms|second=>postman:55.57s,,telescope:55000ms|3rd=>postman:58.51s,telescope:58451ms
+            //chunksize=1000
+            //first=>postman:54.44s,telescope:54383ms|second=>postman:54.45s,telescope:54394ms|3rd=>postman:48.94s,telescope:48895ms
+            //chunksize=1250
+            //first=>postman:47.17s,telescope:47126ms|second=>postman:48.50s,telescope:48460ms|3rd=>postman:53.93s,telescope:53884ms
+            //chunksize=1500
+            //first=>postman:46.10s,telescope:46072ms|second=>postman:47.26s,telescope:47213ms|3rd=>postman:47.17s,telescope:47126ms
+            //chunksize=1700
+            //first=>postman:55.90s,telescope:55828ms|second=>postman:54.32s,telescope:54252ms|3rd=>postman:55.21s,telescope:55165ms
+            //chunkSize=2000
+            //first=>postman:1m6.78s,telescope:66736ms|second=>postman:1m13.98s,telescope:73934ms|3rd=>postman:1m8.40s,telescope:68347ms
+            ExcelDemo::insert($rows);
         }
     }
 
@@ -63,22 +80,31 @@ class ExcelDemoCollectionImport implements ToCollection, WithChunkReading, WithH
      */
     public function chunkSize(): int
     {
-        return 1000;
+        return 1100;
     }
 
+    /**
+     * 自定义方法取出需要的数据
+     *
+     * @param collect $rows
+     * @param array $column_map 标题头和表字段映射
+     * @return array
+     */
     public function covertRowData($rows, $column_map)
     {
-        //原始keys
-        $origin_keys =  array_keys($column_map);
+        $result = [];
+        $i = 0;
+        $now = now();
 
         foreach ($rows as $key => & $row) {
-            $row = $row->only($origin_keys);
             foreach ($column_map as $key => $value) {
-                $row[$value] = $row[$key];
-                unset($row[$key]);
+                $result[$i][$value] = $row[$key];
             }
+            $result[$i]['created_at'] = $now;
+            $result[$i]['updated_at'] = $now;
+            $i++;
         }
 
-        return $row;
+        return $result;
     }
 }
