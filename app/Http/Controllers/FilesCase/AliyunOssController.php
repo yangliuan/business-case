74580e5b2f06d2ca42e220db11e71e4b9e9bd3f7
yangliuancn <yangliuan@foxmail.com>
2021-12-07 22:24:15 +0800

    add

--- app/Http/Controllers/FilesCase/AliyunOssController.php
+++ app/Http/Controllers/FilesCase/AliyunOssController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\FilesCase;
 
 use App\Http\Controllers\Controller;
+use App\Models\PictureDemo;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
@@ -11,11 +12,16 @@ class AliyunOssController extends Controller
     public function store(Request $request)
     {
         $request->validate([
+            'disk'=>'bail|required|in:public,oss',
             'file'=>'bail|required|file',
         ]);
         $path = 'test/'.date('Y-m-d');
-        $res = Storage::disk('oss')->put($path, $request->file('file'));
+        $file_name = Storage::disk($request->input('disk'))->put($path, $request->file('file'));
+        $picture_demo = PictureDemo::create([
+            'disk'=>$request->input('disk'),
+            'path'=>$file_name,
+        ]);
 
-        dd($res);
+        return $picture_demo;
     }
 }
