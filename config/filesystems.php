0fae902a3c4fe9d899544ed0d9d277bde04cf0ab
yangliuan <yangliuancn@foxmail.com>
2021-12-07 19:02:48 +0800

    modify

--- config/filesystems.php
+++ config/filesystems.php
@@ -53,6 +53,46 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
         ],
 
+        'oss' => [
+            'driver' => 'oss',
+            'root' => '', // 设置上传时根前缀
+            'access_key' => env('OSS_ACCESS_KEY'),
+            'secret_key' => env('OSS_SECRET_KEY'),
+            'endpoint'   => env('OSS_ENDPOINT'), // 使用 ssl 这里设置如: https://oss-cn-beijing.aliyuncs.com
+            'bucket'     => env('OSS_BUCKET'),
+            // 如果 isCname 为 false，endpoint 应配置 oss 提供的域名如：`oss-cn-beijing.aliyuncs.com`，
+            // 否则为自定义域名，，cname 或 cdn 请自行到阿里 oss 后台配置并绑定 bucket
+            'isCName'    => env('OSS_IS_CNAME', false),
+            'region_id'  => env('OSS_REGION_ID', ''),
+            'role_arn'   => env('OSS_ROLE_ARN', ''),
+            'policy' => json_encode([
+                "Version" => "1",
+                'Statement' => [
+                    [
+                        "Effect" => "Allow",
+                        "Action" => [
+                            "oss:*"
+                        ],
+                        "Resource" => [
+                            "acs:oss:*:*:penglong-shop",
+                            "acs:oss:*:*:penglong-shop/*"
+                        ],
+                    ]
+                ]
+            ]),
+            // 如果有更多的 bucket 需要切换，就添加所有bucket，默认的 bucket 填写到上面，不要加到 buckets 中
+            'buckets'=>[
+                'test'=>[
+                    'access_key' => env('OSS_ACCESS_KEY'),
+                    'secret_key' => env('OSS_SECRET_KEY'),
+                    'bucket'     => env('OSS_TEST_BUCKET'),
+                    'endpoint'   => env('OSS_TEST_ENDPOINT'),
+                    'isCName'    => env('OSS_TEST_IS_CNAME', false),
+                ],
+                //...
+            ],
+        ],
+
     ],
 
     /*
