03d52d73595384dae0a3a29de8cf3a892da2cf8e
yangliuan <yangliuancn@foxmail.com>
2021-12-07 14:12:09 +0800

    modify

--- app/Http/Controllers/Wechat/MiniProgramController.php
+++ app/Http/Controllers/Wechat/MiniProgramController.php
@@ -1,5 +1,9 @@
 <?php
-
+/**
+ * 本案例使用EasyWechat5.0
+ *
+ * DOC:https://www.easywechat.com/5.x/
+ */
 namespace App\Http\Controllers\Wechat;
 
 use Illuminate\Validation\ValidationException;
@@ -26,19 +30,31 @@ class MiniProgramController extends Controller
 
         $config = config('wechat.mini_program.default');
         $app = Factory::miniProgram($config);
-
+        //auth.code2Session接口
+        //DOC:https://developers.weixin.qq.com/miniprogram/dev/api-backend/open-api/login/auth.code2Session.html
+        //DOC:
         $res_session = $app->auth->session($request->code);
 
         if (isset($res_session['errcode']) && $res_session['errcode']) {
             throw ValidationException::withMessages(['code' => $res_session['errcode'] . $res_session['errmsg']]);
         }
 
+        //解密信息
         try {
             $decryptedData = $app->encryptor->decryptData($res_session['session_key'], $request->iv, $request->encryptedData);
         } catch (\Exception $e) {
             throw ValidationException::withMessages(['encryptedData' => $e->getMessage()]);
         }
 
+        //场景一:微信体系绑定同一个用户的微信号，使用unionid作为唯一标识
+        //DOC:https://developers.weixin.qq.com/miniprogram/dev/framework/open-ability/union-id.html
+        //存在使用code获取的unionid 则使用,不存在使用解密获得的unionid
+        $unionid = isset($res_session['unionid']) && $res_session['unionid'] ? $res_session['unionid']:$decryptedData['unionId'];
+
+        //场景二：使用openid作为微信用户唯一标识
+        $res_session['openid'];
+
+        //用户信息
         $user_info = [
             'name' => $decryptedData['nickName'],
             'avatar' => $decryptedData['avatarUrl'],
