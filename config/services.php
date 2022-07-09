001f57a5d76440fcbd45545c4139117fadb57089
yangliuancn <yangliuan@foxmail.com>
2022-07-09 16:20:05 +0800

    modify

--- config/services.php
+++ config/services.php
@@ -30,4 +30,37 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    //easysms sdk
+    //https://github.com/overtrue/easy-sms
+    'easysms' => [
+        // HTTP 请求的超时时间（秒）
+        'timeout' => 5.0,
+
+        // 默认发送配置
+        'default' => [
+            // 网关调用策略，默认：顺序调用
+            'strategy' => \Overtrue\EasySms\Strategies\OrderStrategy::class,
+
+            // 默认可用的发送网关
+            'gateways' => [
+                'yunpian', 'aliyun',
+            ],
+        ],
+        // 可用的网关配置
+        'gateways' => [
+            'errorlog' => [
+                'file' => '/notify/easy-sms.log',
+            ],
+            'yunpian' => [
+                'api_key' => env('EASYSMS_YUNPIAN_API_KEY',''),
+            ],
+            'aliyun' => [
+                'access_key_id' => env('EASYSMS_ALIYUN_KEY_ID',''),
+                'access_key_secret' => env('EASYSMS_ALIYUN_KEY_SECRET',''),
+                'sign_name' => env('EASYSMS_ALIYUN_SING_NAME',''),
+            ],
+            //...
+        ],
+    ],
+
 ];
